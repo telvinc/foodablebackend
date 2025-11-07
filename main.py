d8452660eb3daded773dfd4c13cc1255d70a321c
Telvin Ciby <tciby@chemeketa.edu>
2025-11-07 14:41:34 -0500

    feat: add CORS middleware and ignore frontend folder

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from database import Base, engine
-from routers import groceries 
+from routers import groceries
+
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Foodable Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(groceries.router)
 
